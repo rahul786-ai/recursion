a8e64520e48da13a0dd99b45da815692b455510e
rahul shah <rkofficial568@gmail.com>
2021-11-26 01:41:25 +0530

    init

--- factorial.c
+++ factorial.c
@@ -6,10 +6,18 @@ int fact(int n)
         return fact(n-1)*n;
     }
 }
+int Isum(int n)
+{
+    int s = 0;
+    int  i;
+    for(i=1; i<=n; i++)
+    s = s+i;
+    return s;
+}
 int main()
 {
     int r;
-    r =  fact(5);
+    r =  fact(1);
     printf("%d", r);
     return 0;
 }

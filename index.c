66d1e563e9569508d31bb253a29bfa1c40425ba1
rahul shah <rkofficial568@gmail.com>
2021-11-26 01:34:16 +0530

    init

--- index.c
+++ index.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int sum(int n)
 {
-    if(n==0)
+    if(n==0){
     return 0;
+    }
     return sum(n-1)+n;
 }
 int Isum(int n)
@@ -11,11 +12,12 @@ int Isum(int n)
     int  i;
     for(i=1; i<=n; i++)
     s = s+i;
-    return s
+    return s;
 }
 int main()
 {
     int r;
-    r = sum(5);
-    printf("%d", r)
+    r = Isum(5);
+    printf("%d", r);
+    return 0;
 }
